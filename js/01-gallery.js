584adb0d29e44fac5e363cf9a287b0281de2bc6a
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2022-11-30 19:38:23 +0200

    30.11

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+`<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`
