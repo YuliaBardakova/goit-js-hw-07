5ecd78f374fdd38ce51112ba73d70bf5253ef052
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2022-12-02 18:51:56 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery')
-const items = []
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = galleryItems
+.map(({ preview, description, original }) =>
+	`<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>`
+	).join('');
 
-galleryItems.forEach(element => {
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-	galleryImage.className = 'gallery__image'
-	galleryImage.src = element.preview
-	galleryImage.setAttribute('title', element.description)
-	galleryImage.alt = element.description
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 
-	galleryLink.append(galleryImage)
-	items.push(galleryLink)
-})
-gallery.append(...items)
-
-new SimpleLightbox('.gallery a', {
-	captionDelay: 250
-});
+new SimpleLightbox('.gallery a', { 
+	captionsData:"alt",
+	captionDelay: 250, 
+	captionPosition: "bottom",});
